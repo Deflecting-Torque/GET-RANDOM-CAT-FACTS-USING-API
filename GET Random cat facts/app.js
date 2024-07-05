a8a07a45824d884b97dfa66c2e69652baa5c927e
Deflecting-Torque <116160070+Deflecting-Torque@users.noreply.github.com>
2024-07-05 12:53:11 +0530

    Add files via upload

---
+++ GET Random cat facts/app.js
@@ -0,0 +1,21 @@
+let btn=document.querySelector("button");
+btn.addEventListener("click",async ()=>{
+    let fact= await getFacts();
+    let p= document.querySelector("p");
+    p.innerText=fact;
+    
+});
+let url="https://catfact.ninja/fact";
+async function getFacts(){
+    try{
+        let res= await axios.get(url);
+        return (res.data.fact);
+        
+
+    }
+    catch(e){
+        console.log("ERROR-",e);
+        return "NO FACT FOUND";
+        
+    }
+}
